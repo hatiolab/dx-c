57bfa9e43b74ebe161150b71b4cf14ef487f2120
heartyoh <heartyoh@hatiolab.com>
2015-06-25 20:00:20 +0900

    playback

--- demo/omnid_host_handler_stream.c
+++ demo/omnid_host_handler_stream.c
@@ -12,9 +12,27 @@
 
 #include "omnid.h"
 
+time_t demo_stream_first_received_time = 0;
+time_t demo_stream_last_received_time = 0;
+int demo_stream_frames_received = 0;
+
 int od_host_handler_stream(int fd, dx_packet_t* packet) {
 	dx_stream_packet_t* stream = (dx_stream_packet_t*)packet;
+	int framerate = 0;
+	time_t now = time(NULL);
 
-	printf("Stream Received : size %d\n", htonl(stream->data.len));
+	if(now - demo_stream_last_received_time > 1) {
+		/* reset all */
+		demo_stream_first_received_time = now;
+		demo_stream_last_received_time = now;
+		demo_stream_frames_received++;
+	} else {
+		demo_stream_frames_received++;
+		if(demo_stream_frames_received != 0 && now != demo_stream_first_received_time)
+			framerate = (demo_stream_frames_received) / (now - demo_stream_first_received_time);
+		demo_stream_last_received_time = now;
+	}
+
+	printf("Stream Received : %d, framerate %d, size (%d).\n", demo_stream_frames_received, framerate, htonl(stream->data.len));
 }
 
